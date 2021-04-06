68c9113f3869d9a9de011bff9fcf4b5d86fba886
lksowerbutts <lksowerbutts@wpi.edu>
2021-04-06 18:21:42 -0400

    added controller and methods to switch to gift delivery form and back

--- src/main/java/edu/wpi/teamname/views/ServiceRequests.java
+++ src/main/java/edu/wpi/teamname/views/ServiceRequests.java
@@ -1,3 +1,43 @@
 package edu.wpi.teamname.views;
 
-public class ServiceRequests {}
+import edu.wpi.teamname.App;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+
+import java.io.IOException;
+
+public class ServiceRequests {
+    public void floralDelivery(ActionEvent actionEvent) {
+    }
+
+    public void foodDelivery(ActionEvent actionEvent) {
+    }
+
+    public void medicineDelivery(ActionEvent actionEvent) {
+    }
+
+    public void giftDelivery(ActionEvent actionEvent) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/edu/wpi/teamname/views/GiftDelivery.fxml"));
+            App.getPrimaryStage().getScene().setRoot(root);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void sanitationServices(ActionEvent actionEvent) {
+    }
+
+    public void laundryServices(ActionEvent actionEvent) {
+    }
+
+    public void patientTransportation(ActionEvent actionEvent) {
+    }
+
+    public void facilitiesMaintenance(ActionEvent actionEvent) {
+    }
+
+    public void computerService(ActionEvent actionEvent) {
+    }
+}
