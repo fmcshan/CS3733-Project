bc3458492804b192063679a0012f85d8e76a220a
Anthony4668 <anthony4668@gmail.com>
2021-04-10 15:35:42 -0400

    Add Default page

---
+++ src/main/java/edu/wpi/teamname/views/DefaultPage.java
@@ -0,0 +1,36 @@
+package edu.wpi.teamname.views;
+
+import edu.wpi.teamname.App;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+
+import java.io.IOException;
+
+public class DefaultPage {
+
+    public void loadScene(String fileName) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/edu/wpi/teamname/views/" + fileName + ".fxml"));
+            App.getPrimaryStage().getScene().setRoot(root);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void pathFinding(ActionEvent actionEvent) {
+    }
+
+    public void edgesEditor(ActionEvent actionEvent) {
+    }
+
+    public void nodesEditor(ActionEvent actionEvent) {
+    }
+
+    public void serviceRequests(ActionEvent actionEvent) {
+        loadScene("ServiceRequests");
+    }
+
+    public void exitApplication(ActionEvent actionEvent) {
+    }
+}
