2ce29222436acd6da125dafaade33c3bdb2df4d5
Bryan <bryank123@live.com>
2021-04-11 16:03:29 -0400

    Import A* algorithm over from https://github.com/CS3733-D21-TEAM-L/PathfindingAlgorithms . Import includes a parser, Node structure, A*, tests and subsequent classes to make previously mentioned things work

---
+++ src/main/java/edu/wpi/teamname/Algo/AStar.java
@@ -0,0 +1,160 @@
+package edu.wpi.teamname.Algo;
+
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.Stack;
+
+/**
+ * <h1>AStar Pathfinding</h1>
+ * AStar Pathfinding Algorithm that navigates a map of provided nodes
+ * @author Emmanuel Ola
+ */
+public class AStar {
+    /**
+     * ArrayList of provided nodes
+     */
+    private ArrayList nodes;
+    /**
+     * The starting node
+     */
+    private Node start;
+    /**
+     * The ending node
+     */
+    private Node goal;
+    /**
+     * Priority queue of open nodes. Instantiated with our NodeComparator class
+     */
+    private PriorityQueue<Node> openNodes;
+
+    /** Loads the relevant information for the AStar algorithm and prints the result of the pathfinding operation
+     * @param nodes the list of nodes
+     * @param start the starting node
+     * @param goal the ending node
+     */
+    public AStar(ArrayList nodes, Node start, Node goal) {
+        this.nodes = nodes;
+        this.start = start;
+        start.setCostSoFar(0); //Initializes the cost so far of the starting node to 0
+        this.goal = goal;
+        openNodes = new PriorityQueue<>(new NodeComparator()); //Instantiates the priority queue with our overwrited comparator
+        this.process();
+        this.displayPath();
+    }
+
+    /**
+     * Prints the path from the start node to the goal node
+     */
+    public void displayPath(){
+        Stack<Node> finalPath = this.getPath(); //
+        while (!finalPath.isEmpty())
+            System.out.println(finalPath.pop().getNodeInfo().get("fullName"));
+    }
+
+    /**
+     * Helper method for displayPath() and returnPath() that provides a stack containing the solution path
+     * @return Stack containing the solution path for algorithm
+     */
+    private Stack<Node> getPath() {
+        Stack<Node> finalPath = new Stack<>(); //Stack containing the final path of our algorithm
+
+
+        Node current = goal;
+        while (current.getParent() != null){
+            finalPath.push(current);
+            current = current.getParent();
+        }
+        finalPath.push(start); //Pushes the starting node on to the stack
+
+        return finalPath;
+    }
+
+    /**
+     * <b>*For JUnit Testing*</b> This method returns a list of nodes from start to finish that represents
+     * the path of the AStar algorithm
+     * @return a List of nodes representing the path of the algorithm
+     */
+    public ArrayList<Node> returnPath(){
+        Stack<Node> finalPath = this.getPath();
+        ArrayList<Node> returnPath = new ArrayList<Node>();
+        while (!finalPath.isEmpty())
+            returnPath.add(finalPath.pop());
+        return returnPath;
+    }
+
+    /**
+     * Navigates the map by processing the priority queue
+     */
+    public void process(){
+        openNodes.add(start); //Adds our starting node to the priority queue
+        Node current = start; //Sets the current node to the starting node
+        double tentativeScore = 0;
+
+        while (!openNodes.isEmpty()){
+            if (current.getNodeID().equals(goal.getNodeID()))
+                return; //Ends pathfinding when we reach our goal
+            current = openNodes.poll();
+
+            //Iterates through the neighbors of each node popped from the Priority Queue
+            for (Node neighbor : current.getEdges().keySet()) {
+                tentativeScore = (current.getCostSoFar() + distance(current, neighbor)); //Sets tentative score to the cost of taking this path to the neighbor
+                if (tentativeScore < neighbor.getCostSoFar()){ //Checks if the current path to neighbor is cheaper than any other we've encountered
+                    neighbor.setParent(current);
+                    neighbor.setCostSoFar(tentativeScore);
+                    neighbor.setHeuristic(calculateHeuristic(neighbor));
+                    neighbor.updateAStarScore();
+                    if (!openNodes.contains(neighbor))
+                        openNodes.add(neighbor); //Adds neighbor to the priority queue if it isn't already in there
+                }
+            }
+        }
+    }
+
+    /**
+     * Calculates the heuristic for a provided node
+     * @param node the node to calculate a heuristic for
+     * @return the heuristic for the provide node
+     */
+    public double calculateHeuristic(Node node) {
+        return distance(node, goal);
+    }
+
+    /**
+     * Calculates the distance from one node to another
+     * @param a the starting node
+     * @param b the ending node
+     * @return the euclidean distance between two nodes
+     */
+    public double distance(Node a, Node b) {
+        //TODO MAKE THIS A SWITCH STATEMENT/MAKE DISTANCE FORMULA 3D
+        int x1 = a.getCoords().get("x");
+        int x2 = b.getCoords().get("x");
+        int y1 = a.getCoords().get("y");
+        int y2 = b.getCoords().get("y");
+        double flatDistance = Math.sqrt((Math.pow((x2-x1),2)) + (Math.pow((y2-y1),2)));
+        String afloor = a.getNodeInfo().get("floor");
+        String bfloor = b.getNodeInfo().get("floor");
+        if (afloor.equals("L1")||afloor.equals("L2"))
+            if (afloor.equals("L1"))
+                afloor = "0";
+            else
+                afloor = "-1";
+        if (bfloor.equals("L1")||bfloor.equals("L2"))
+            if (bfloor.equals("L1"))
+                bfloor = "0";
+            else
+                bfloor = "-1";
+        return flatDistance + (Math.abs(Integer.valueOf(afloor) - Integer.valueOf(bfloor)) * 50) ;
+        //return flatDistance;
+    }
+
+    public static void main(String[] args) {
+        //TODO make nodeID take click instance/drop-down  menu
+        Stopwatch timer = new Stopwatch();
+        ArrayList<Node> nodes = Parser.loadNodesandEdges();
+        Node start = nodes.get(Parser.indexOfNode(nodes, "EEXIT00201"));
+        Node goal = nodes.get(Parser.indexOfNode(nodes, "GDEPT00403"));
+        AStar example = new AStar(nodes, start, goal);
+        System.out.println(timer.elapsedTime());
+    }
+}
---
+++ src/main/java/edu/wpi/teamname/Algo/Parser.java
@@ -0,0 +1,118 @@
+package edu.wpi.teamname.Algo;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * <h1>Parser</h1>
+ * This program parses csv files into nodes ArrayLists containing info about each node.
+ * It also loads the edges connected to each node
+ * @author Emmanuel Ola
+ */
+
+public class Parser {
+    /**
+     * Method to read in csv files and convert them to a list of row elements
+     * containing info for each file
+     * @param path Path to the csv file
+     * @author Freud Oulon
+     * @return A nested list of strings representing the different rows of data from the csv files
+     */
+    public static List<List<String>> readFile(String path) {
+        List<List<String>> allElements = new ArrayList<>();
+        List<String> elementData;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(path));
+            String line;
+            boolean skip = true;
+            while ((line = br.readLine()) != null) {
+                if (skip) {
+                    skip = false;
+                } else {
+                    String[] values = line.split(",");
+                    elementData = Arrays.asList(values);
+                    allElements.add(elementData);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return allElements;
+    }
+
+    /**
+     * Static method that reads the data passed from Parser.readFile() and converts
+     * them into an ArrayList of nodes that information about each node.
+     * It also loads the edges of each node while it parses the data
+     * @author emmanuelola
+     * @return ArrayList of bidirectional nodes in the CSV file
+     */
+    public static ArrayList<Node> loadNodesandEdges() {
+        List<List<String>> nodesData = Parser.readFile(System.getProperty("user.dir") + "/nodes.csv");
+        List<List<String>> edgesData = Parser.readFile(System.getProperty("user.dir") + "/edges.csv");
+        ArrayList<Node> nodesList = new ArrayList<>(); //Instantiate the Arraylist of nodes
+        //Load all the nodes into nodesList
+        for (List<String> edgesDatum : edgesData) {
+            String nodeID1 = edgesDatum.get(1);
+            loadNode(nodesData, nodesList, nodeID1);
+            String nodeID2 = edgesDatum.get(2);
+            loadNode(nodesData, nodesList, nodeID2);
+        }
+        //Loads the edges for each node
+        for (Node node : nodesList) {
+            for (List<String> edgesDatum : edgesData) {
+                if (node.getNodeID().equals(edgesDatum.get(1))){
+                    node.addEdges(nodesList.get(Parser.indexOfNode(nodesList, edgesDatum.get(2))), 1);
+                    nodesList.get(Parser.indexOfNode(nodesList, edgesDatum.get(2))).addEdges(node, 1);
+                }
+            }
+        }
+        return nodesList;
+    }
+
+    /**
+     * A helper method that loads all the data regarding a node into the node
+     * @param nodesData the raw nodes data provided by the CSV reader
+     * @param nodesList the list of nodes processed by the parser
+     * @param nodeID1 the nodeID of the node we want to load (from the edges data)
+     */
+    private static void loadNode(List<List<String>> nodesData, ArrayList<Node> nodesList, String nodeID1) {
+        if (Parser.indexOfNode(nodesList, nodeID1) == -1){
+            for (int i = 0; i < nodesData.size(); i++) {
+                List<String> nodeData = nodesData.get(i);
+                String nodeID = nodeData.get(0);
+                if (nodeID.equals(nodeID1)){
+                    int x = Integer.valueOf(nodeData.get(1));
+                    int y = Integer.valueOf(nodeData.get(2));
+                    String floor = nodeData.get(3);
+                    String building = nodeData.get(4);
+                    String nodeType = nodeData.get(5);
+                    String longName = nodeData.get(6);
+                    String shortName = nodeData.get(7);
+                    nodesList.add(new Node(nodeID, x, y, floor, building, nodeType, longName, shortName));
+                }
+            }
+        }
+    }
+
+    /**
+     * Retrieves the index of a node within an ArrayList of nodes, when provided the nodeID for the node.
+     * @param nodes ArrayList of nodes
+     * @param nodeID the Node ID of the node you're looking for
+     * @author Emmanuel Ola
+     * @return index of provided node, or -1 if the node isn't in the list
+     */
+    public static int indexOfNode(ArrayList<Node> nodes, String nodeID) {
+        for (int i = 0; i < nodes.size(); i++) {
+            if (nodes.get(i).getNodeID().equals(nodeID))
+                return i;
+        }
+        return -1;
+    }
+}
+
+
