0d7ddeb564ed7fa123ec361a01bc639fbefdf79b
jlkarol <81386092+jlkarol@users.noreply.github.com>
2021-04-12 00:01:15 -0400

    Add comments

--- src/main/java/edu/wpi/teamname/Database/PathFindingDatabaseManager.java
+++ src/main/java/edu/wpi/teamname/Database/PathFindingDatabaseManager.java
@@ -2,11 +2,16 @@ package edu.wpi.teamname.Database;
 
 import com.google.api.core.ApiFuture;
 import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.firestore.*;
-
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.WriteBatch;
+import com.google.cloud.firestore.WriteResult;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
+import edu.wpi.teamname.Algo.Node;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.*;
 import java.net.HttpURLConnection;
@@ -16,30 +21,73 @@ import java.util.concurrent.ExecutionException;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-import edu.wpi.teamname.Algo.Node;
-import edu.wpi.teamname.Algo.Parser;
-import org.json.JSONArray;
-import org.json.JSONObject;
-
 
 public class PathFindingDatabaseManager {
     private static final PathFindingDatabaseManager instance = new PathFindingDatabaseManager();
-
-    private PathFindingDatabaseManager() {
-    }
-
-    ;
     private Firestore db;
 
-    public void startDb() {
-        initDb();
-        System.out.println(getNodes());
+    private PathFindingDatabaseManager() {
     }
 
     public static synchronized PathFindingDatabaseManager getInstance() {
         return instance;
     }
 
+    /**
+     * Splits a list into a list of lists with a maximum size of length
+     *
+     * @param source Source list
+     * @param length Maximum list length/batch size
+     * @return List of lists with a maximum size of length
+     */
+    private static <T> Stream<List<T>> batches(List<T> source, int length) {
+        if (length <= 0)
+            throw new IllegalArgumentException("length = " + length);
+        int size = source.size();
+        if (size <= 0)
+            return Stream.empty();
+        int fullChunks = (size - 1) / length;
+        return IntStream.range(0, fullChunks + 1).mapToObj(
+                n -> source.subList(n * length, n == fullChunks ? size : (n + 1) * length));
+    }
+
+    private static ArrayList<Node> parseNodes(JSONArray nodesData, JSONArray edgeData) {
+        ArrayList<Node> nodesList = new ArrayList<>();
+        HashMap<String, Node> nodeMap = new HashMap<String, Node>();
+        for (int n = 0; n < nodesData.length(); n++) {
+            JSONObject node = nodesData.getJSONObject(n);
+            Node newNode = new Node(
+                    node.getString("id"),
+                    Integer.parseInt(node.getString("x")),
+                    Integer.parseInt(node.getString("y")),
+                    node.getString("level"),
+                    node.getString("building"),
+                    node.getString("type"),
+                    node.getString("longName"),
+                    node.getString("shortName")
+            );
+            nodeMap.put(node.getString("id"), newNode);
+            nodesList.add(newNode);
+        }
+
+        for (int n = 0; n < edgeData.length(); n++) {
+            JSONObject edge = edgeData.getJSONObject(n);
+            if (nodeMap.containsKey(edge.getString("start")) && nodeMap.containsKey(edge.getString("end"))) {
+                Node startNode = nodeMap.get(edge.getString("start"));
+                Node endNode = nodeMap.get(edge.getString("start"));
+                startNode.addEdges(endNode, 1);
+            }
+        }
+        return nodesList;
+    }
+
+    public void startDb() {
+        initDb();
+    }
+
+    /**
+     * Initializes the firestore database
+     */
     private void initDb() {
         try {
             InputStream serviceAccount = new FileInputStream(System.getProperty("user.dir") + "/3733-firebase-service-account.json");
@@ -56,17 +104,9 @@ public class PathFindingDatabaseManager {
         }
     }
 
-    private static <T> Stream<List<T>> batches(List<T> source, int length) {
-        if (length <= 0)
-            throw new IllegalArgumentException("length = " + length);
-        int size = source.size();
-        if (size <= 0)
-            return Stream.empty();
-        int fullChunks = (size - 1) / length;
-        return IntStream.range(0, fullChunks + 1).mapToObj(
-                n -> source.subList(n * length, n == fullChunks ? size : (n + 1) * length));
-    }
-
+    /**
+     * Read the provided CSV and insert nodes into the database
+     */
     private void insertNodesIntoDatabase() {
         List<List<String>> allNodesData = CSVReader.readFile(System.getProperty("user.dir") + "/L1Nodes.csv");
         Set<List<String>> nodesDataAsSet = new HashSet<>(allNodesData); // to avoid duplicate elements
@@ -105,8 +145,9 @@ public class PathFindingDatabaseManager {
         });
     }
 
-    ;
-
+    /**
+     * Read the provided CSV and insert edges into the database
+     */
     public void insertEdgesIntoDatabase() {
         List<List<String>> allEdgesData = CSVReader.readFile(System.getProperty("user.dir") + "/L1Edges.csv");
         Set<List<String>> edgesDataAsSet = new HashSet<>(allEdgesData); // to avoid duplicate elements
@@ -139,6 +180,12 @@ public class PathFindingDatabaseManager {
         });
     }
 
+    /**
+     * Performs a get request and returns the response as a json object
+     * @param _url The URL to get
+     * @return A json object
+     * @throws IOException
+     */
     public JSONObject getRequestJson(String _url) throws IOException {
         URL url = new URL(_url);
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
@@ -156,6 +203,10 @@ public class PathFindingDatabaseManager {
         return new JSONObject(response.toString());
     }
 
+    /**
+     * Nodes are handled internally as a json object
+     * @return A json array of nodes.
+     */
     private JSONArray getNodesInteral() {
         ArrayList<List<String>> nodes = new ArrayList<>();
         try {
@@ -167,6 +218,10 @@ public class PathFindingDatabaseManager {
         return new JSONArray();
     }
 
+    /**
+     * Edges are handled internally as a json object
+     * @return A json array of edges.
+     */
     private JSONArray getEdgesInternal() {
         try {
             JSONObject nodeList = getRequestJson("https://us-central1-software-engineering-3733.cloudfunctions.net/get-edges?cache=true");
@@ -177,38 +232,10 @@ public class PathFindingDatabaseManager {
         return new JSONArray();
     }
 
-    private static ArrayList<Node> parseNodes(JSONArray nodesData, JSONArray edgeData) {
-        ArrayList<Node> nodesList = new ArrayList<>();
-        HashMap<String, Node> nodeMap = new HashMap<String, Node>();
-        for(int n = 0; n < nodesData.length(); n++)
-        {
-            JSONObject node = nodesData.getJSONObject(n);
-            Node newNode = new Node(
-                    node.getString("id"),
-                    Integer.parseInt(node.getString("x")),
-                    Integer.parseInt(node.getString("y")),
-                    node.getString("level"),
-                    node.getString("building"),
-                    node.getString("type"),
-                    node.getString("longName"),
-                    node.getString("shortName")
-            );
-            nodeMap.put(node.getString("id"), newNode);
-            nodesList.add(newNode);
-        }
-
-        for(int n = 0; n < edgeData.length(); n++)
-        {
-            JSONObject edge = edgeData.getJSONObject(n);
-            if (nodeMap.containsKey(edge.getString("start")) && nodeMap.containsKey(edge.getString("end"))) {
-                Node startNode = nodeMap.get(edge.getString("start"));
-                Node endNode = nodeMap.get(edge.getString("start"));
-                startNode.addEdges(endNode, 1);
-            }
-        }
-        return nodesList;
-    }
-
+    /**
+     * Return an arraylist of node classes
+     * @return An arraylist of node classes
+     */
     public ArrayList<Node> getNodes() {
         JSONArray nodes = getNodesInteral();
         JSONArray edges = getEdgesInternal();
