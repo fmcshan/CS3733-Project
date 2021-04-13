0b8a9a4affc6a933153ddf861adae90898b6b674
Freud <foulon@wpi.edu>
2021-04-13 00:54:53 -0400

    Node csv added

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -5,9 +5,9 @@ import edu.wpi.teamname.Database.PathFindingDatabaseManager;
 public class Main {
 
   public static void main(String[] args) {
-  //  App.launch(App.class, args);
-PathFindingDatabaseManager.getInstance().startDb();
-    PathFindingDatabaseManager.getInstance().getNodes();
+  App.launch(App.class, args);
+//PathFindingDatabaseManager.getInstance().startDb();
+//    PathFindingDatabaseManager.getInstance().getNodes();
 
   }
 }
