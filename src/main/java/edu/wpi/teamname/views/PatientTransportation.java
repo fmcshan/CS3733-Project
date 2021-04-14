a2af6cefc758f7d7619d39e053f311f8fc6ccddf
Xianhan Jia <xjia@wpi.edu>
2021-04-14 23:43:02 +0800

    complete PatientTransportation service request

--- src/main/java/edu/wpi/teamname/views/PatientTransportation.java
+++ src/main/java/edu/wpi/teamname/views/PatientTransportation.java
@@ -7,18 +7,19 @@ import javafx.fxml.FXML;
 
 import java.util.ArrayList;
 
-public class FacilitiesMaintenance extends MasterRequest {
+public class PatientTransportation extends MasterRequest{
 
     ArrayList<Node> listOfNodes = new ArrayList<>();
     @FXML
     private JFXComboBox<String> comboBox;
+    @FXML
+    private JFXComboBox<String> comboBox1;
     public void initialize(){
         listOfNodes = PathFindingDatabaseManager.getInstance().getNodes();
 
         listOfNodes.forEach(n -> {
             comboBox.getItems().add(n.getLongName());
+            comboBox1.getItems().add(n.getLongName());
         });
     }
-
-
 }
