5eeb53b48c98dcc246164637e4a69be10e95b9b6
Freud <foulon@wpi.edu>
2021-04-14 11:19:04 -0400

    Maintenance Service Request initial Controller implemented

--- src/main/java/edu/wpi/teamname/views/FacilitiesMaintenance.java
+++ src/main/java/edu/wpi/teamname/views/FacilitiesMaintenance.java
@@ -1,6 +1,26 @@
 package edu.wpi.teamname.views;
 
+import com.jfoenix.controls.JFXComboBox;
+import edu.wpi.teamname.Algo.Node;
+import edu.wpi.teamname.Database.PathFindingDatabaseManager;
+import javafx.fxml.FXML;
+
+import java.util.ArrayList;
+
 public class FacilitiesMaintenance extends MasterRequest {
+
+    ArrayList<Node> listOfNodes = new ArrayList<>();
+    @FXML
+    private JFXComboBox<String> comboBox;
+    public void initialize(){
+        listOfNodes = PathFindingDatabaseManager.getInstance().getNodes();
+
+        listOfNodes.forEach(n -> {
+            //nodesMap.put(n.getNodeID(), n);
+            comboBox.getItems().add(n.getLongName());
+            //fromCombo.getItems().add(n.getNodeID());
+        });
+    }
 
 
 }
