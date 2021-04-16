0d4dc4b3a8a317b7b1f0e7462113a68bc09d485a
Bryan <bryank123@live.com>
2021-04-16 09:04:56 -0400

    Make changes to zoom and pan. Currently not functional Bryan's pushes

--- src/main/java/edu/wpi/teamname/views/Zoom.java
+++ src/main/java/edu/wpi/teamname/views/Zoom.java
@@ -1,17 +1,19 @@
 package edu.wpi.teamname.views;
-
+import javafx.application.Application;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
-import javafx.fxml.FXML;
-import javafx.geometry.Orientation;
-import javafx.geometry.Point2D;
-import javafx.geometry.Rectangle2D;
-import javafx.scene.control.Label;
-import javafx.scene.control.Slider;
+import javafx.geometry.*;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 public class Zoom {
 
@@ -22,6 +24,20 @@ public class Zoom {
         map.setPreserveRatio(false); //make sure that the image (the map) is bound to its original image dimensions (aka the aspect ratio)
 
         ObjectProperty<Point2D> mouseClickDown = new SimpleObjectProperty<>();
+
+        map.setOnMousePressed(mouseEvent -> {
+            Point2D pointOfMouseClick = viewportToImage(map, new Point2D(mouseEvent.getX(), mouseEvent.getY()));
+            mouseClickDown.set(pointOfMouseClick);
+        });
+
+        map.setOnMouseDragged(mouseEvent -> {
+            Point2D pointToDragFrom = viewportToImage(map, new Point2D(mouseEvent.getX(), mouseEvent.getY()));
+            Point2D valueOfShift = pointToDragFrom.subtract(mouseClickDown.get());
+            shiftedImage(map, valueOfShift);
+            mouseClickDown.set(viewportToImage(map, new Point2D(mouseEvent.getX(), mouseEvent.getY())));
+        });
+
+
 
     }
 }
