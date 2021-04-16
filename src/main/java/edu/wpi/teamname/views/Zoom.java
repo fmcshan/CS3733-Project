f01b04d22ba725b61be4813769ac4641468ccd41
Bryan <bryank123@live.com>
2021-04-16 09:05:16 -0400

    Make changes to zoom and pan. Currently not functional Xianhan's push

--- src/main/java/edu/wpi/teamname/views/Zoom.java
+++ src/main/java/edu/wpi/teamname/views/Zoom.java
@@ -40,4 +40,58 @@ public class Zoom {
 
 
     }
+
+    public static Point2D viewportToImage(ImageView inputMap, Point2D mapCoordinates) {
+        Bounds bounds = inputMap.getBoundsInLocal();
+        double xProportion = mapCoordinates.getX() / bounds.getWidth();
+        double yProportion =mapCoordinates.getY() / bounds.getHeight();
+
+        Rectangle2D viewport = inputMap.getViewport();
+        return new Point2D(viewport.getMinX() + xProportion * viewport.getWidth(), viewport.getMinY() + yProportion * viewport.getHeight());
+    }
+
+    private static double calculateEnsureRange(double value, double min, double max) {
+        return Math.min(Math.max(value, min), max);
+    }
+
+    private static boolean inRange(double value, double min, double max) {
+        return (value>= min) && (value<= max);
+    }
+
+    private static double ensureRange(double value, double min, double max) {
+        boolean isInRange = inRange(value, min, max);
+        double returnVal = -1.0;
+
+        if (isInRange){
+            returnVal = calculateEnsureRange(value, min, max);
+        } else {
+            system.out.println("You messed up");
+            returnVal = calculateEnsureRange(value, min, max);
+        }
+
+        return returnVal;
+    }
+
+    public static void shiftedImage(ImageView inputMap, Point2D changeInShift) {
+        Rectangle2D theViewPort = inputMap.getViewport();
+
+        //Extracting the image's height and width
+        double imageWidth = inputMap.getImage().getWidth();
+        double imageHeight = inputMap.getImage().getHeight();
+
+        double viewportMaxWidth = imageWidth - theViewPort.getWidth();
+        double viewportMaxHeight = imageHeight - theViewPort.getHeight();
+
+        double viewportMinXCoord = theViewPort.getMinX();
+        double viewportMinYCoord = theViewPort.getMinY();
+
+        double changeInX = changeInShift.getX();
+        double changeInY = changeInShift.getY();
+
+        double viewportMinWidth = ensureRange(viewportMinXCoord - changeInX, 0, viewportMaxWidth);
+        double viewportMinHeight = ensureRange(viewportMinYCoord - changeInY, 0, viewportMaxHeight);
+
+        inputMap.setViewport(new Rectange2D());
+    }
+
 }
