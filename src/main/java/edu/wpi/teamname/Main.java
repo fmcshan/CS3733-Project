0e174a4877e378b1bacc40e1d3ee48dddde9c04d
fmcshan <fmcshan@wpi.edu>
2021-04-17 16:56:10 -0400

    -Initial commit -Add AsynchronousTask to account for tasks -Add AsynchronousQueue to process Asynchronous Tasks

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -1,11 +1,15 @@
 package edu.wpi.teamname;
 
+import edu.wpi.teamname.Database.AsynchronousQueue;
+import edu.wpi.teamname.Database.AsynchronousTask;
 import edu.wpi.teamname.Database.DatabaseThread;
 
 public class Main {
 
     public static void main(String[] args) {
         DatabaseThread.getInstance().start();
+        AsynchronousQueue.getInstance().start();
+
         App.launch(App.class, args);
     }
 }
