99c2073bd262bdb5602b48c07becc1ab978bb4c8
eoola <eoola@wpi.edu>
2021-04-17 09:59:27 -0400

    Add JUnit Tests for AStar.java

--- src/main/java/edu/wpi/teamname/Algo/AStar.java
+++ src/main/java/edu/wpi/teamname/Algo/AStar.java
@@ -36,7 +36,7 @@ public class AStar {
      * @param goal the ending node
      */
     public AStar(ArrayList nodes, Node start, Node goal) {
-        this.resetNodes(nodes);
+        this.resetNodes(nodes); //reset all provided nodes before pathfinding
         this.start = start;
         start.setCostSoFar(0); //Initializes the cost so far of the starting node to 0
         this.goal = goal;
@@ -45,6 +45,10 @@ public class AStar {
         this.displayPath();
     }
 
+    /**
+     * Resets the parents and costs of all nodes in the provided arraylist of nodes
+     * @param nodes an ArrayList of Nodes to be resetted
+     */
     private void resetNodes(ArrayList<Node> nodes) {
         for (Node node : nodes) {
             if (node.getParent() != null) {
