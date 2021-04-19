e2536217c87ee73e8b1872bd2307a542cd5aee9c
eoola <eoola@wpi.edu>
2021-04-19 19:07:36 -0400

    Connect the GiftDeliveryRequest to the UI so it can be opened in a pop-up window

--- src/main/java/edu/wpi/teamname/views/GiftDeliveryRequest.java
+++ src/main/java/edu/wpi/teamname/views/GiftDeliveryRequest.java
@@ -3,13 +3,19 @@ package edu.wpi.teamname.views;
 import com.jfoenix.controls.JFXCheckBox;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
+import edu.wpi.teamname.Algo.Node;
+import edu.wpi.teamname.Database.PathFindingDatabaseManager;
 import edu.wpi.teamname.Entities.ServiceRequests.GiftRequest;
 import edu.wpi.teamname.Entities.ServiceRequests.ServiceRequest;
+import edu.wpi.teamname.bridge.Bridge;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -99,12 +105,33 @@ public class GiftDeliveryRequest {
     private VBox successPop;
 
     /**
+     * Instance of Requests class used to create a popup window
+     */
+    @FXML
+    private Requests request;
+
+    /**
      * List of Service Requests
      */
     private List<ServiceRequest> requests;
 
     /**
+     * Constructor used to create a pop up window for GiftDelivery Request
+     * @param request an instance of Requests.java
+     */
+    public GiftDeliveryRequest(Requests request) {
+        this.request = request;
+    }
+
+    public void initialize() {
+        for (Node node : PathFindingDatabaseManager.getInstance().getNodes()) {
+            requestLocation.getItems().add(node.getNodeID());
+        }
+    }
+
+    /**
      * Retrieves sucess pop up page
+     *
      * @return Success pop up page
      */
     public VBox getSuccessPop() {
@@ -113,27 +140,48 @@ public class GiftDeliveryRequest {
 
     /**
      * Checks if first name and last name have been filled in correctly
+     *
      * @return true if there's a space between the first and last name, and false otherwise
      */
-    public boolean nameInputValid() { return nameInput.getText().contains(" ");}
+    public boolean nameInputValid() {
+        return nameInput.getText().contains(" ");
+    }
 
     /**
      * Checks if a checkbox has been selected
+     *
      * @return true if any checkbox has been selected, and false otherwise
      */
-    public boolean checkBoxSelected() { return teddyBearBox.isSelected() || chocolateBox.isSelected() || giftBasketBox.isSelected() || otherCheckbox.isSelected();}
+    public boolean checkBoxSelected() {
+        return teddyBearBox.isSelected() || chocolateBox.isSelected() || giftBasketBox.isSelected() || otherCheckbox.isSelected();
+    }
 
     /**
      * Checks if the "Other" text box for gift delivery options has been filled correctly
+     *
      * @return true if the box was filled correctly, and false otherwise
      */
-    public boolean otherInputValid() { return !otherCheckbox.isSelected() || (otherCheckbox.isSelected() && !otherInput.getText().isEmpty());}
+    public boolean otherInputValid() {
+        return !otherCheckbox.isSelected() || (otherCheckbox.isSelected() && !otherInput.getText().isEmpty());
+    }
 
     /**
      * Checks if the phone number text field has been filled in correctly
+     *
      * @return true if the number matches the "XXX-XXX-XXXX" format
      */
-    public boolean phoneNumberValid() { return phoneInput.getText().matches("\\d{3}-\\d{3}-\\d{4}");}
+    public boolean phoneNumberValid() {
+        return phoneInput.getText().matches("\\d{3}-\\d{3}-\\d{4}");
+    }
+
+    /**
+     * Checks if a location has been selected correctly
+     *
+     * @return true if a location has been selected, and false otherwise
+     */
+    public boolean locationValid() {
+        return !requestLocation.getValue().isEmpty();
+    }
 
     public void addRequest(ServiceRequest request) {
         requests.add(request);
@@ -146,14 +194,16 @@ public class GiftDeliveryRequest {
      *     <li>Creates a new gift request object</li>
      *     <li>Adds the gift request to the service request database</li>
      * </ul>
+     *
      * @param event event triggering submission
      */
     @FXML
     void submitRequest(ActionEvent event) {
         if (phoneInput.getText().length() == 10 && !phoneInput.getText().contains("-")) {
-            phoneInput.setText(phoneInput.getText().substring(0,3) +  "-" + phoneInput.getText().substring(3,6) + "-" + phoneInput.getText().substring(6));
+            phoneInput.setText(phoneInput.getText().substring(0, 3) + "-" + phoneInput.getText().substring(3, 6) + "-" + phoneInput.getText().substring(6));
         }
 
+        //Checks if all the inputs are valid
         if (!nameInputValid())
             failedName.setText("Invalid Name Entry. Make sure to input first and last name.");
         else
@@ -171,17 +221,21 @@ public class GiftDeliveryRequest {
         else
             failedGiftSelection.setText("");
 
-
         if (!phoneNumberValid())
             failedPhoneNumber.setText("Please enter your phone number in the \"XXX-XXX-XXXX\" format ");
         else
             failedPhoneNumber.setText("");
 
+        if (!locationValid())
+            failedLocationEntry.setText("Please select a location");
+
         if (requests == null) {
             requests = new ArrayList<ServiceRequest>();
         }
 
-        if (nameInputValid() && checkBoxSelected() && otherInputValid() && phoneNumberValid()){
+
+        if (nameInputValid() && checkBoxSelected() && otherInputValid() && phoneNumberValid()) {
+            //Adds all the selected gifts to an arraylist
             ArrayList<String> giftSelected = new ArrayList<>();
             if (teddyBearBox.isSelected())
                 giftSelected.add("Teddy Bear");
@@ -191,9 +245,38 @@ public class GiftDeliveryRequest {
                 giftSelected.add("Gift Basket");
             if (otherCheckbox.isSelected())
                 giftSelected.add(otherInput.getText());
-            requests.add(new GiftRequest(phoneInput.getText(), requestLocation.getValue(), nameInput.getText(), giftSelected));
-        }
 
+            //Add this request to our list of requests
+            requests.add(new GiftRequest(phoneInput.getText(), requestLocation.getValue(), nameInput.getText(), giftSelected));
+
+            //Close the window after submitting the request
+            request.getRequestPop().getChildren().clear(); //Clear the request pop VBox
+            Bridge.getInstance().close(); //close the window
+        }
     }
 
+    /**
+     * Load Request form when the button is pressed/make it disappear
+     */
+    public void loadRequest() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/edu/wpi/teamname/views/GiftDeliveryRequest.fxml"));
+        try {
+            loader.setControllerFactory(type -> {
+                if (type == GiftDeliveryRequest.class)
+                    return this;
+                else
+                    try {
+                        return type.newInstance();
+                    } catch (RuntimeException e) {
+                        throw e;
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+            });
+            Parent root = loader.load();
+            request.openWindowRequestPop("requestForm", root); //open/close request form
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
