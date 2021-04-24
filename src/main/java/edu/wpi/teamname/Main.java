94d2808d9d460f485d8e7e52e6f9c3d92e60f22c
jlkarol <81386092+jlkarol@users.noreply.github.com>
2021-04-23 23:50:37 -0400

    Increase fps

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -10,6 +10,7 @@ import java.util.UUID;
 public class Main {
 
     public static void main(String[] args) {
+        System.setProperty("javafx.animation.fullspeed", "true");
         Config.getInstance().setEnv("staging"); // dev staging production
         SocketManager.getInstance().startDataSocket();
         AuthenticationManager.getInstance().loginWithEmailAndPassword("admin@admin.com", "password");
