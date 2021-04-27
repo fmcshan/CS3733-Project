5ff2c546d9351e28cb3bed2059bd2da1d01f6f44
Anthony4668 <anthony4668@gmail.com>
2021-04-27 01:35:35 -0400

    make actions employee table

--- src/main/java/edu/wpi/teamname/views/EmployeeTable.java
+++ src/main/java/edu/wpi/teamname/views/EmployeeTable.java
@@ -1,9 +1,12 @@
 package edu.wpi.teamname.views;
 
 import com.jfoenix.controls.JFXButton;
+import edu.wpi.teamname.Authentication.User;
 import edu.wpi.teamname.Database.LocalStorage;
 import edu.wpi.teamname.Database.Submit;
 import edu.wpi.teamname.Database.socketListeners.Initiator;
+import edu.wpi.teamname.Database.socketListeners.UserListener;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Side;
@@ -16,12 +19,17 @@ import javafx.scene.layout.VBox;
 
 import java.io.IOException;
 
-public class EmployeeTable {
+public class EmployeeTable implements UserListener {
 
     @FXML
     private VBox cellHolder;
 
     public void initialize() {
+        Initiator.getInstance().addUserListener(this);
+        populateTable();
+    }
+
+    private void populateTable() {
         LocalStorage.getInstance().getUsers().forEach(r -> {
             try {
                 Node node = FXMLLoader.load(getClass().getResource("/edu/wpi/teamname/views/EmployeeTableCells.fxml"));
@@ -53,36 +61,34 @@ public class EmployeeTable {
                                     if (!r.isAdmin()) {
                                         label.setStyle("-fx-background-color: #f13426; -fx-background-radius: 4px");
                                     }
-                                    System.out.println(r.isAdmin());
                                 });
                                 break;
                             case "actionsBox":
-                                System.out.println("made it here");
                                 vbox.getChildren().forEach(v -> {
                                     JFXButton button = (JFXButton) v;
                                     ContextMenu contextMenu = new ContextMenu();
-                                    MenuItem makeAdmin = new MenuItem("make admin");
-                                    MenuItem revokeAdmin = new MenuItem("revoke admin");
-                                    MenuItem delete = new MenuItem("delete");
-                                    contextMenu.getItems().add(makeAdmin);
-                                    contextMenu.getItems().add(revokeAdmin);
+                                    MenuItem makeAdmin = new MenuItem("Make Admin");
+                                    MenuItem revokeAdmin = new MenuItem("Revoke Admin");
+                                    MenuItem delete = new MenuItem("Delete");
+                                    if (!r.isAdmin()) {
+                                        contextMenu.getItems().add(makeAdmin);
+                                    } else {
+                                        contextMenu.getItems().add(revokeAdmin);
+                                    }
                                     contextMenu.getItems().add(delete);
                                     button.setOnAction(b -> {
                                         contextMenu.show(button, Side.BOTTOM, 0, 0);
                                     });
                                     contextMenu.setOnAction(e -> {
                                         switch (((MenuItem) e.getTarget()).getText()) {
-                                            case "make admin":
-//                                                 _emp.isAdmin() = true;
-//                                                Submit.getInstance().updateGiftDelivery(_emp);
+                                            case "Make Admin":
+                                                Submit.getInstance().grantAdmin(r);
                                                 break;
-                                            case "revoke admin":
-//                                                _emp.isAdmin() = false;
-//                                                Submit.getInstance().updateGiftDelivery(_emp);
+                                            case "Revoke Admin":
+                                                Submit.getInstance().revokeAdmin(r);
                                                 break;
-                                            case "delete":
-//                                                Submit.getInstance().deleteGiftDelivery(_req);
-//                                                Submit.getInstance().updateGiftDelivery(_emp);
+                                            case "Delete":
+                                                Submit.getInstance().deleteUser(r);
                                                 break;
                                         }
                                     });
@@ -94,6 +100,28 @@ public class EmployeeTable {
                 });
             } catch (IOException ex) {
                 ex.printStackTrace();
+            }
+        });
+    }
+
+    @Override
+    public void refreshUsers() {
+        cellHolder.getChildren().clear();
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                populateTable();
+            }
+        });
+    }
+
+    @Override
+    public void updateUser(User _user) {
+        cellHolder.getChildren().clear();
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                populateTable();
             }
         });
     }
