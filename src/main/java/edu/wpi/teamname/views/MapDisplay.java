974ea374ffe43262f8d1976699a1f52bb61ee9a3
Anthony4668 <anthony4668@gmail.com>
2021-04-27 22:12:33 -0400

    file name changes - refactor

--- src/main/java/edu/wpi/teamname/views/MapDisplay.java
+++ src/main/java/edu/wpi/teamname/views/MapDisplay.java
@@ -1150,7 +1150,7 @@ public class MapDisplay implements LevelChangeListener {
         clearMap(); // Clear map
         //  currentPath= new ArrayList();
         popPop.setPrefWidth(657.0); // Set preferable width to 350
-        LoadFXML.getInstance().loadWindow("Requests2", "reqBar", popPop); // Load requests window
+        LoadFXML.getInstance().loadWindow("Requests", "reqBar", popPop); // Load requests window
     }
 
 
