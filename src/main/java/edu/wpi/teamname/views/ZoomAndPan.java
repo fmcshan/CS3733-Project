1e136bb186924c74f35708b847e3103dd0cabe46
jlkarol <81386092+jlkarol@users.noreply.github.com>
2021-05-02 13:37:36 -0400

    Revert "Refactor map editor - edges between floors"
    This reverts commit 858f362089b1bb00b8047e36c6dfca2ce70e32a4.

--- src/main/java/edu/wpi/teamname/views/ZoomAndPan.java
+++ src/main/java/edu/wpi/teamname/views/ZoomAndPan.java
@@ -71,6 +71,7 @@ public class ZoomAndPan {
         page.onTopOfTopElements.setOnMouseDragged(mouseEvent -> {
             dragged = true;
             if (mouseEvent.getTarget() instanceof Circle) {
+                dragged = false; //this should be here right?
                 return;
             }
 
