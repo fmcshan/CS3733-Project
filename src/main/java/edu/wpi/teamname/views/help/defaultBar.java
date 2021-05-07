c5de262505a15e3d42bdf09ee327f1a9ffdbc551
lksowerbutts <lksowerbutts@wpi.edu>
2021-05-07 16:13:09 -0400

    add About page

--- src/main/java/edu/wpi/teamname/views/help/defaultBar.java
+++ src/main/java/edu/wpi/teamname/views/help/defaultBar.java
@@ -2,10 +2,20 @@ package edu.wpi.teamname.views.help;
 
 import edu.wpi.teamname.views.LoadFXML;
 import edu.wpi.teamname.views.manager.SceneManager;
+import javafx.event.ActionEvent;
 
 public class defaultBar {
+
+    public void openAbout(ActionEvent actionEvent) {
+        SceneManager.getInstance().getDefaultPage().setHelpButton(false);
+        SceneManager.getInstance().getDefaultPage().getPopPop().getChildren().clear();
+        LoadFXML.getInstance().loadWindow("AboutPage", "aboutPage", SceneManager.getInstance().getDefaultPage().getPopPop());
+    }
+
     public void closeWindow() {
         SceneManager.getInstance().getDefaultPage().getPopPop().getChildren().clear();
         LoadFXML.setCurrentHelp("");
+        LoadFXML.setCurrentWindow("");
+        SceneManager.getInstance().getDefaultPage().setHelpButton(true);
     }
 }
