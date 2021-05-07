25da1223c8a41117668ea178a5de0b98d4f6cea5
fmcshan <fmcshan@wpi.edu>
2021-05-07 15:08:20 -0400

    Add credits button to admin default help page

--- src/main/java/edu/wpi/teamname/views/help/defaultSignOutBar.java
+++ src/main/java/edu/wpi/teamname/views/help/defaultSignOutBar.java
@@ -2,10 +2,14 @@ package edu.wpi.teamname.views.help;
 
 import edu.wpi.teamname.views.LoadFXML;
 import edu.wpi.teamname.views.manager.SceneManager;
+import javafx.event.ActionEvent;
 
 public class defaultSignOutBar {
     public void closeWindow() {
         SceneManager.getInstance().getDefaultPage().getPopPop().getChildren().clear();
         LoadFXML.setCurrentHelp("");
+    }
+
+    public void openCredits(ActionEvent actionEvent) {
     }
 }
