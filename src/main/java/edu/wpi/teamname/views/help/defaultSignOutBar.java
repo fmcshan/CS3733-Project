a74ee18dda2632506fc4c75a379bb382a5477829
fmcshan <fmcshan@wpi.edu>
2021-05-07 16:16:10 -0400

    Add credits page

--- src/main/java/edu/wpi/teamname/views/help/defaultSignOutBar.java
+++ src/main/java/edu/wpi/teamname/views/help/defaultSignOutBar.java
@@ -8,8 +8,13 @@ public class defaultSignOutBar {
     public void closeWindow() {
         SceneManager.getInstance().getDefaultPage().getPopPop().getChildren().clear();
         LoadFXML.setCurrentHelp("");
+        LoadFXML.setCurrentWindow("");
+        SceneManager.getInstance().getDefaultPage().setHelpButton(true);
     }
 
     public void openCredits(ActionEvent actionEvent) {
+        SceneManager.getInstance().getDefaultPage().setHelpButton(false);
+        SceneManager.getInstance().getDefaultPage().getPopPop().getChildren().clear();
+        LoadFXML.getInstance().loadWindow("Credits", "Credits", SceneManager.getInstance().getDefaultPage().getPopPop());
     }
 }
